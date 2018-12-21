190f05014ed5f162c44a338cb1c6c7c16bbb9bef
Fabian Küng <fabian.kueng@inpeek.ch>
2018-12-21 09:23:27 +0100

    Basic structure

---
+++ src/backend/data/Account.java
@@ -0,0 +1,5 @@
+package backend.data;
+
+public abstract class Account {
+
+}
---
+++ src/frontend/atm/Main.java
@@ -0,0 +1,10 @@
+package frontend.atm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}

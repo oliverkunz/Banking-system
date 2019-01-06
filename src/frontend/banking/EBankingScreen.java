52fa842c18214b54317411804b1134ce1e3bde1e
Oliver Kunz <oli-97@bluewin.ch>
2019-01-06 21:33:23 +0100

    EBanking Kontoübersicht

--- src/frontend/banking/EBankingScreen.java
+++ src/frontend/banking/EBankingScreen.java
@@ -3,6 +3,7 @@ package frontend.banking;
 import backend.api.Account;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -17,22 +18,25 @@ public class EBankingScreen extends Pane implements ChangeListener<Account> {
 		this.setId("EBankingScreen");
 			
 		VBox vbox = new VBox(10.0);
-		vbox.setMinHeight(100);
-		vbox.setMaxHeight(250);
+		vbox.setMinHeight(200);
+		vbox.setMaxHeight(350);
+		vbox.setPadding(new Insets(250, 0, 25, 100));
 
-		table = new TableView<Account>();
-		TableColumn<Account, Long> idCol = new TableColumn<Account, Long>("Konto");
-		idCol.setCellValueFactory(new PropertyValueFactory<Account, Long>("Konto"));
-		idCol.setMinWidth(180);
-		TableColumn<Account, String> titleCol = new TableColumn<Account, String>("Betrag");
-		titleCol.setCellValueFactory(new PropertyValueFactory<Account, String>("Betrag"));
-		titleCol.setMinWidth(250);
-		table.getColumns().add(idCol);
-		table.getColumns().add(titleCol);
+		TableView<Account> table = new TableView<Account>();
+		TableColumn<Account, String> accountCol = new TableColumn<Account, String>("Konto");
+		accountCol.setCellValueFactory(new PropertyValueFactory<Account, String>("Konto"));
+		accountCol.setMinWidth(350);
+		TableColumn<Account, Double> amountCol = new TableColumn<Account, Double>("Betrag");
+		amountCol.setCellValueFactory(new PropertyValueFactory<Account, Double>("Betrag"));
+		amountCol.setMinWidth(250);
+		table.getColumns().add(accountCol);
+		table.getColumns().add(amountCol);
 
 		table.getSelectionModel().selectedItemProperty().addListener(this);
 		vbox.getChildren().add(table);
 		vbox.setAlignment(Pos.CENTER);
+		
+		getChildren().addAll(vbox);
 	}
 
 	public TableView<Account> getTable() {
